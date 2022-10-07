8b8b50f938e403a762d94209a1e9e16e80cc0420
kallagoutham <kallagoutham33@gmail.com>
2022-10-07 16:05:16 +0530

    Project Setup

---
+++ src/main/java/Spring/ValueMediBE/ValueMediBeApplication.java
@@ -0,0 +1,13 @@
+package Spring.ValueMediBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueMediBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueMediBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring/ValueMediBE/ValueMediBeApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.ValueMediBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueMediBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
